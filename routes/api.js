c4bfe2434ef381f6de143066a6182a8f1857041e
Kareem James <kareemdjames@gmail.com>
2018-01-24 11:17:58 -0500

    able to fetch jobs from db

--- routes/api.js
+++ routes/api.js
@@ -5,24 +5,41 @@ const router = vertex.router()
 
 /*  This is a sample API route. */
 
-router.get('/:resource', function(req, res){
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		query: req.query // from the url query string
-	})
-})
+// router.get('/:resource', function(req, res){
+// 	res.json({
+// 		confirmation: 'success',
+// 		resource: req.params.resource,
+// 		query: req.query // from the url query string
+// 	})
+// })
 
-router.get('/:resource/:id', function(req, res){
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		id: req.params.id,
-		query: req.query // from the url query string
-	})
-})
+// router.get('/:resource/:id', function(req, res){
+// 	res.json({
+// 		confirmation: 'success',
+// 		resource: req.params.resource,
+// 		id: req.params.id,
+// 		query: req.query // from the url query string
+// 	})
+// })
 
 // Job Route
+router.get('/job', function(req, res){
+	// Fetches all jobs	
+	turbo.fetch('job', null)
+		.then(data => {
+			res.json({
+				confirmation: 'success',
+				data: data
+			})
+		})
+		.catch(err => {
+			res.json({
+				confirmation: 'fail',
+				message: err.message
+			})
+		})
+	})
+
 router.post('/job', function(req, res){
 	// Creating job in turbo datastore
 	turbo.create('job', req.body)
