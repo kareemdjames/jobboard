7a16c7f3e697ec72a907415f7442ec68e87596e2
Kareem James <kareemdjames@gmail.com>
2018-01-24 09:16:11 -0500

    initial app creation, turbo deploy successful, created new feed component. setup for Redux out the box will add in future.

---
+++ README.md
@@ -0,0 +1,7 @@
+# job-board
+
+This project was built with Turbo 360. To learn more, click here: https://www.turbo360.co
+
+## Instructions
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import store from './stores'
+
+const app = (
+	<div>
+		<h1>Job Board App!</h1>	
+	</div>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'))
